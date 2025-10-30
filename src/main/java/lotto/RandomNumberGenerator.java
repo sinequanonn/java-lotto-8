faa65c40968adc66369c8ddedb31c0c24f6aae37
sinequanonn <dleck288@gmail.com>
2025-10-30 22:12:05 +0900

    docs: 로또 한 개 발행 구현 완료

--- src/main/java/lotto/RandomNumberGenerator.java
+++ src/main/java/lotto/RandomNumberGenerator.java
@@ -5,7 +5,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class RandomNumberGenerator {
-
     public static final int START_NUMBER = 1;
     public static final int END_NUMBER = 45;
     public static final int NUMBER_COUNT = 6;
