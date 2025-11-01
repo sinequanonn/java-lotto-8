8a4ebc14fb4dd303e387d41fd37a445e3a6f5d85
sinequanonn <dleck288@gmail.com>
2025-11-01 17:24:43 +0900

    feat: 로또 당첨 등수 계산 기능 구현

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,45 @@
+package lotto.domain;
+
+public enum LottoRank {
+    FIRST(2_000_000_000, "6개 일치"),
+    SECOND(30_000_000,"5개 일치, 보너스 볼 일치"),
+    THIRD(1_500_000, "5개 일치"),
+    FOURTH(50_000, "4개 일치"),
+    FIFTH(5_000, "3개 일치"),
+    NONE(0, "0개 일치");
+
+    private long money;
+    private String message;
+
+    LottoRank(long money, String message) {
+        this.money = money;
+        this.message = message;
+    }
+
+    public static LottoRank of(int matchingCount, boolean bonusMatch) {
+        if (matchingCount == 6) {
+            return FIRST;
+        }
+        if (matchingCount == 5) {
+            if (bonusMatch) {
+                return SECOND;
+            }
+            return THIRD;
+        }
+        if (matchingCount == 4) {
+            return FOURTH;
+        }
+        if (matchingCount == 3) {
+            return FIFTH;
+        }
+        return NONE;
+    }
+
+    public long getMoney() {
+        return money;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
