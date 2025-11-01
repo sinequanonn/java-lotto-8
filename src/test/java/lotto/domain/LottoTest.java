b3f097a04008d11d5433e97ef2bdb8147138d533
sinequanonn <dleck288@gmail.com>
2025-11-01 16:02:35 +0900

    test: 당첨 번호와 로또 번호가 일치하는 개수 반환 테스트 작성

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -19,5 +20,25 @@ class LottoTest {
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_번호와_일치하는_번호_개수를_계산한다() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumber = List.of(1, 2, 3, 7, 8, 9);
+
+        //when&then
+        assertThat(lotto.countMatch(winningNumber)).isEqualTo(3);
+    }
+
+    @Test
+    void 일치하는_번호가_없으면_0을_반환한다() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumber = List.of(10, 11, 12, 13, 14, 15);
+
+        //when&then
+        assertThat(lotto.countMatch(winningNumber)).isEqualTo(0);
     }
 }
