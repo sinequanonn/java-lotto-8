a4e1155b731bea3baa0113cd37734fe0073f63c4
sinequanonn <dleck288@gmail.com>
2025-11-01 20:15:39 +0900

    feat: 보너스 번호 생성 기능 구현

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+public class BonusNumber {
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+
+    private Integer number;
+
+    public BonusNumber(Integer number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(Integer number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호 숫자 범위는 1에서 45 사이어야 합니다.");
+        }
+    }
+}
