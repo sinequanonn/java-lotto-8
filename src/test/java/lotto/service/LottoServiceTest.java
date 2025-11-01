7e62dd3b31254888ed8000e858deabc6626fc209
sinequanonn <dleck288@gmail.com>
2025-11-01 22:43:33 +0900

    test: LottoService 로또 당첨 결과 계산 테스트 작성

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,14 +1,14 @@
 package lotto.service;
 
-import lotto.domain.Lotto;
-import lotto.domain.LottoMachine;
-import lotto.domain.RandomNumberGenerator;
+import lotto.domain.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -40,5 +40,106 @@ public class LottoServiceTest {
         assertThatThrownBy(() -> lottoService.purchaseLottos(money))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+    }
+
+    @Test
+    void 로또_번호가_6개_일치하면_1등이다() {
+        //given
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = new BonusNumber(7);
+
+        //when
+        Map<LottoRank, Integer> result = lottoService.calculateResult(lottos, winningNumber, bonusNumber);
+
+        //then
+        assertThat(result.get(LottoRank.FIRST)).isEqualTo(1);
+    }
+
+    @Test
+    void 로또_번호가_5개_일치하고_보너스_번호도_일치하면_2등이다() {
+        //given
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 7)));
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = new BonusNumber(7);
+
+        //when
+        Map<LottoRank, Integer> result = lottoService.calculateResult(lottos, winningNumber, bonusNumber);
+
+        //then
+        assertThat(result.get(LottoRank.SECOND)).isEqualTo(1);
+    }
+
+    @Test
+    void 로또_번호가_5개_일치하고_보너스_번호가_일치하지_않으면_3등이다() {
+        //given
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 5, 8)));
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = new BonusNumber(7);
+
+        //when
+        Map<LottoRank, Integer> result = lottoService.calculateResult(lottos, winningNumber, bonusNumber);
+
+        //then
+        assertThat(result.get(LottoRank.THIRD)).isEqualTo(1);
+    }
+
+    @Test
+    void 로또_번호가_4개_일치하면_4등이다() {
+        //given
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 4, 8, 9)));
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = new BonusNumber(7);
+
+        //when
+        Map<LottoRank, Integer> result = lottoService.calculateResult(lottos, winningNumber, bonusNumber);
+
+        //then
+        assertThat(result.get(LottoRank.FOURTH)).isEqualTo(1);
+    }
+
+    @Test
+    void 로또_번호가_3개_일치하면_5등이다() {
+        //given
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 3, 8, 9, 10)));
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = new BonusNumber(7);
+
+        //when
+        Map<LottoRank, Integer> result = lottoService.calculateResult(lottos, winningNumber, bonusNumber);
+
+        //then
+        assertThat(result.get(LottoRank.FIFTH)).isEqualTo(1);
+    }
+
+    @Test
+    void 로또_번호가_2개_이하로_일치하면_꽝이다() {
+        //given
+        List<Lotto> lottos = List.of(new Lotto(List.of(1, 2, 7, 8, 9, 10)));
+        List<Integer> winningNumber = List.of(1, 2, 13, 14, 15, 16);
+        BonusNumber bonusNumber = new BonusNumber(45);
+
+        //when
+        Map<LottoRank, Integer> result = lottoService.calculateResult(lottos, winningNumber, bonusNumber);
+
+        //then
+        assertThat(result.get(LottoRank.NONE)).isEqualTo(1);
+    }
+
+    @Test
+    void 같은_등수가_여러_개_있을_떄_개수를_센다() {
+        //given
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(1, 2, 3, 8, 9, 10)),
+                new Lotto(List.of(1, 2, 3, 8, 9, 10)),
+                new Lotto(List.of(1, 2, 3, 8, 9, 10)));
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        BonusNumber bonusNumber = new BonusNumber(7);
+
+        //when
+        Map<LottoRank, Integer> result = lottoService.calculateResult(lottos, winningNumber, bonusNumber);
+
+        //then
+        assertThat(result.get(LottoRank.FIFTH)).isEqualTo(3);
     }
 }
