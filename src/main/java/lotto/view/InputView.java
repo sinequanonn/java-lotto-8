d5e33645b6ed4310047e9dccca3da367b8a570b9
sinequanonn <dleck288@gmail.com>
2025-11-02 03:23:41 +0900

    refactor: InputView 기능 추가 및 변경
    - 공백 출력 메서드 추가
    - 입력 메시지 한 번 출력되도록 메서드 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -14,6 +14,7 @@ public class InputView {
     public static final String INPUT_WINNING_LOTTO_MESSAGE = "당첨 번호를 입력해주세요.";
     public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해주세요.";
     private static final String NUMBER_REGEX = "\\d+";
+    public static final String DELIMITER = ",";
 
     public void printInputMoneyMessage() {
         System.out.println(INPUT_MONEY_MESSAGE);
@@ -25,19 +26,31 @@ public class InputView {
         return Integer.parseInt(input);
     }
 
-    public List<Integer> inputWinningLotto() {
+    public void printInputWinningLotto() {
+        printBlankLine();
         System.out.println(INPUT_WINNING_LOTTO_MESSAGE);
+    }
+
+    public List<Integer> inputWinningLotto() {
         String input = Console.readLine();
         List<Integer> winningNumbers = parseWinningNumbers(input);
         validateWinningNumbers(winningNumbers);
         return winningNumbers;
     }
 
-    public Integer inputBonusNumber() {
+    public void printInputBonusNumber() {
+        printBlankLine();
         System.out.println(INPUT_BONUS_NUMBER);
+    }
+
+    public Integer inputBonusNumber() {
         String input = Console.readLine();
         validateIntegerNumber(input);
         return Integer.parseInt(input);
+    }
+
+    public void printBlankLine() {
+        System.out.println();
     }
 
     private void validateWinningNumbers(List<Integer> winningNumbers) {
@@ -46,18 +59,16 @@ public class InputView {
     }
 
     private List<Integer> parseWinningNumbers(String input) {
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(DELIMITER))
                 .map(String::trim)
                 .peek(this::validateIntegerNumber)
                 .map(Integer::parseInt)
                 .toList();
     }
 
-
-
     private void validateIntegerNumber(String input) {
         if (!input.matches(NUMBER_REGEX)) {
-            throw new IllegalArgumentException("[ERROR] 정수이어야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 입력 값은 정수이어야 합니다.");
         }
     }
 }
