278d561ceca828d767bbf23a4d5a9dac9c74328d
sinequanonn <dleck288@gmail.com>
2025-11-02 01:15:29 +0900

    feat: Application 메인 클래스 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.LottoMachine;
+import lotto.domain.RandomNumberGenerator;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        LottoMachine lottoMachine = new LottoMachine(randomNumberGenerator);
+        LottoService lottoService = new LottoService(lottoMachine);
+        LottoController lottoController = new LottoController(inputView, outputView, lottoService);
+
+        lottoController.run();
     }
 }
