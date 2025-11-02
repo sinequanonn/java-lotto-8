c7f5d938945663dfb307de379559b21eadd832a7
sinequanonn <dleck288@gmail.com>
2025-11-02 15:27:12 +0900

    refactor: LottoMachine 수정
    - 무상태를 위해 coin, usedMoney 필드 제거
    - 코인을 생성하고 사용 로직을 비즈니스 로직으로 분리

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,46 +1,13 @@
 package lotto.domain;
 
-import lotto.exception.ErrorMessage;
-
 public class LottoMachine {
-    private static final int LOTTO_PRICE = 1000;
-
     private final RandomNumberGenerator generator;
-    private int coin;
-    private int usedMoney;
 
     public LottoMachine(RandomNumberGenerator generator) {
         this.generator = generator;
-        this.coin = 0;
-        this.usedMoney = 0;
-    }
-
-    public void exchangeForCoins(int money) {
-        validateExchangeAmount(money);
-        int purchasedCoins = money / LOTTO_PRICE;
-        coin += purchasedCoins;
-        usedMoney += money;
     }
 
     public Lotto issueLotto() {
         return new Lotto(generator.generate());
-    }
-
-    public void useCoin() {
-        coin--;
-    }
-
-    public boolean hasCoins() {
-        return coin > 0;
-    }
-
-    public int getUsedMoney() {
-        return usedMoney;
-    }
-
-    private void validateExchangeAmount(int money) {
-        if (money % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_PRICE.getMessage());
-        }
     }
 }
--- src/test/java/lotto/domain/LottoMachineTest.java
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoMachineTest {
     private LottoMachine lottoMachine;
@@ -15,47 +14,11 @@ public class LottoMachineTest {
     }
 
     @Test
-    void 로또기계에_구입금액을_넣으면_로또를_발행할_수_있는_코인을_생성한다() {
-        //given
-        int money = 1000;
-
+    void 로또를_한_개_생생한다() {
         //when
-        lottoMachine.exchangeForCoins(money);
-
-        //then
-        assertThat(lottoMachine.hasCoins()).isTrue();
-    }
-
-    @Test
-    void 구입금액이_로또금원_단위가_아니면_예외가_발생한다() {
-        //given
-        int money = 1500;
-
-        //when & then
-        assertThatThrownBy(() -> lottoMachine.exchangeForCoins(money))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
-    }
-
-    @Test
-    void 코인이_있으면_로또를_발행할_수_있다() {
-        //when
-        lottoMachine.exchangeForCoins(1000);
         Lotto lotto = lottoMachine.issueLotto();
 
         //then
-        assertThat(lotto).isNotNull();
         assertThat(lotto).isInstanceOf(Lotto.class);
-    }
-
-    @Test
-    void 로또를_발행하면_코인이_1개_차감된다() {
-        //when
-        lottoMachine.exchangeForCoins(1000);
-        lottoMachine.issueLotto();
-        lottoMachine.useCoin();
-
-        //then
-        assertThat(lottoMachine.hasCoins()).isFalse();
     }
 }
